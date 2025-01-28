b03e34be5a7d39fa7319ce2e680091b279558e5e
Ludovic H <114417005+Lu2ovic@users.noreply.github.com>
2025-01-28 18:59:38 +0100

    Update donnee_cereales.py

--- donnee_cereales.py
+++ donnee_cereales.py
@@ -1,31 +1,31 @@
 formes_cereales = {
     8: {"nom": "flocon", "nom_anglais": "flake", "prix": 0.06, "description": "Thin, flat, delicate pieces resembling leaves."},
     7: {"nom": "boule", "nom_anglais": "ball", "prix": 0.08, "description": "Small, crunchy spheres perfect for adding texture."},
-    20: {"nom": "anneau", "nom_anglais": "ring", "prix": 0.15, "description": "Circular shapes with a hole in the center, ideal for floating in milk."},
+    11: {"nom": "anneau", "nom_anglais": "ring", "prix": 0.15, "description": "Circular shapes with a hole in the center, ideal for floating in milk."},
     9: {"nom": "cube", "nom_anglais": "cube", "prix": 0.10, "description": "Compact, square pieces, adding a geometric, crunchy look."},
-    29: {"nom": "bâtonnet", "nom_anglais": "stick", "prix": 0.12, "description": "Long, thin shapes resembling crunchy wood pieces."},
-    30: {"nom": "grain soufflé", "nom_anglais": "puffed grain", "prix": 0.18, "description": "Light, airy grains that fill the space with volume."},
-    31: {"nom": "pépite", "nom_anglais": "nugget", "prix": 0.20, "description": "Small, irregular pieces offering a dense, crunchy texture."},
-    21: {"nom": "étoile", "nom_anglais": "star", "prix": 0.22, "description": "Star-shaped pieces, fun and appealing to children."},
+    12: {"nom": "bâtonnet", "nom_anglais": "stick", "prix": 0.12, "description": "Long, thin shapes resembling crunchy wood pieces."},
+    13: {"nom": "grain soufflé", "nom_anglais": "puffed grain", "prix": 0.18, "description": "Light, airy grains that fill the space with volume."},
+    14: {"nom": "pépite", "nom_anglais": "nugget", "prix": 0.20, "description": "Small, irregular pieces offering a dense, crunchy texture."},
+    15: {"nom": "étoile", "nom_anglais": "star", "prix": 0.22, "description": "Star-shaped pieces, fun and appealing to children."},
     10: {"nom": "pétale", "nom_anglais": "petal", "prix": 0.22, "description": "Curved, delicate shapes resembling soft, light petals."}
 }
 
 gouts_cereales = {
-    22: {"nom": "chocolat", "nom_anglais": "chocolate (brown in color)", "prix": 0.06, "description": "Rich cocoa flavor with dark brown shades resembling dark chocolate."},
-    32: {"nom": "vanille", "nom_anglais": "vanilla (beige in color)", "prix": 0.10, "description": "Soft, creamy flavor with a beige or ivory hue resembling vanilla pods."},
-    25: {"nom": "miel", "nom_anglais": "honey (yellow in color)", "prix": 0.12, "description": "Natural sweetness with a golden, shiny color resembling nectar."},
-    28: {"nom": "fruits rouges", "nom_anglais": "red fruits (red in color)", "prix": 0.13, "description": "Lively, fruity flavor with shades of red and pink like strawberries and raspberries."},
-    26: {"nom": "caramel", "nom_anglais": "caramel (amber in color)", "prix": 0.14, "description": "Sweet, buttery flavor with an amber or golden brown color resembling melted caramel."},
-    33: {"nom": "cannelle", "nom_anglais": "cinnamon (brown-orange in color)", "prix": 0.17, "description": "Spicy, warm flavor with brown and orange hues reminiscent of cinnamon bark."},
-    34: {"nom": "noisette", "nom_anglais": "hazelnut (light brown in color)", "prix": 0.22, "description": "Rich, earthy flavor with light brown or golden shades resembling roasted hazelnuts."},
-    27: {"nom": "spéculoos", "nom_anglais": "speculoos (orange-brown in color)", "prix": 0.22, "description": "Sweet, spiced flavor with orange-brown hues, like speculoos cookies."},
-    23: {"nom": "nature", "nom_anglais": "plain (beige in color)", "prix": 0.06, "description": "Simple, pure flavor with light beige or golden tones, highlighting authenticity."},
-    24: {"nom": "sucrée", "nom_anglais": "sweet (white or golden in color)", "prix": 0.08, "description": "Subtle sweetness with white or golden highlights resembling sugar crystals."}
+    18: {"nom": "chocolat", "nom_anglais": "chocolate (brown in color)", "prix": 0.06, "description": "Rich cocoa flavor with dark brown shades resembling dark chocolate."},
+    19: {"nom": "vanille", "nom_anglais": "vanilla (beige in color)", "prix": 0.10, "description": "Soft, creamy flavor with a beige or ivory hue resembling vanilla pods."},
+    20: {"nom": "miel", "nom_anglais": "honey (yellow in color)", "prix": 0.12, "description": "Natural sweetness with a golden, shiny color resembling nectar."},
+    21: {"nom": "fruits rouges", "nom_anglais": "red fruits (red in color)", "prix": 0.13, "description": "Lively, fruity flavor with shades of red and pink like strawberries and raspberries."},
+    22: {"nom": "caramel", "nom_anglais": "caramel (amber in color)", "prix": 0.14, "description": "Sweet, buttery flavor with an amber or golden brown color resembling melted caramel."},
+    23: {"nom": "cannelle", "nom_anglais": "cinnamon (brown-orange in color)", "prix": 0.17, "description": "Spicy, warm flavor with brown and orange hues reminiscent of cinnamon bark."},
+    24: {"nom": "noisette", "nom_anglais": "hazelnut (light brown in color)", "prix": 0.22, "description": "Rich, earthy flavor with light brown or golden shades resembling roasted hazelnuts."},
+    25: {"nom": "spéculoos", "nom_anglais": "speculoos (orange-brown in color)", "prix": 0.22, "description": "Sweet, spiced flavor with orange-brown hues, like speculoos cookies."},
+    26: {"nom": "nature", "nom_anglais": "plain (beige in color)", "prix": 0.06, "description": "Simple, pure flavor with light beige or golden tones, highlighting authenticity."},
+    27: {"nom": "sucrée", "nom_anglais": "sweet (white or golden in color)", "prix": 0.08, "description": "Subtle sweetness with white or golden highlights resembling sugar crystals."}
 }
 
 categorie_cereales = {
-    10: {"nom": "bio", "prix": 0.70, "description": "Natural, earthy look with green and brown tones, evoking leaves or wheat."},
-    5: {"nom": "sport", "prix": 0.60, "description": "Dynamic, modern style with vibrant colors like blue or red, symbolizing energy."},
-    4: {"nom": "gourmand", "prix": 0.54, "description": "Rich, appetizing design with warm, inviting tones, perfect for indulgence."}
+    13: {"nom": "bio", "prix": 0.70, "description": "Natural, earthy look with green and brown tones, evoking leaves or wheat."},
+    12: {"nom": "sport", "prix": 0.60, "description": "Dynamic, modern style with vibrant colors like blue or red, symbolizing energy."},
+    11: {"nom": "gourmand", "prix": 0.54, "description": "Rich, appetizing design with warm, inviting tones, perfect for indulgence."}
 }
 
